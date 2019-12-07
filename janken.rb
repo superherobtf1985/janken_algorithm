97fe49ff5fb105b56e875e51ddc558bb193906ee
shunto <s1610360@edu.cc.uec.ac.jp>
2019-12-07 10:15:47 +0900

    :recycle: 正規表現を使って変更

--- janken.rb
+++ janken.rb
@@ -1,28 +1,21 @@
-def change_int(input)
-  return Integer(input)
-rescue
-  return input
-end
-
 while true do
   hand = ["グー","チョキ","パー"]
   puts "最初はグー、じゃんけん...\n[0]:グー\n[1]:チョキ\n[2]:パー"
-  input = change_int(gets)
+  input = gets
 
-  if input.kind_of?(String)
-    puts '数値を入力してください'
-  else
+  if input =~ /^[0-2]$/
+    input = Integer(input)
     output = rand(3)
     if input == output
       puts "あなたの手:#{hand[input]}、相手の手:#{hand[output]}\nアイコです"
     elsif (input == 0 && output == 1) || (input == 1 && output == 2) || (input == 2 && output == 0)
       puts "あなたの手:#{hand[input]}、相手の手:#{hand[output]}\nあなたの勝ちです"
-      exit
+      break
     elsif (input == 1 && output == 0) || (input == 2 && output == 1) || (input == 0 && output == 2)
       puts "あなたの手:#{hand[input]}、相手の手:#{hand[output]}\nあなたの負けです"
-      exit
-    else
-      puts "入力された値が無効です。"
+      break
     end
+  else
+    puts '0-2の数値を入力してください'
   end
 end
